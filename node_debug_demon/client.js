f41b31bd2320524b330565a30130f22bfe74079c
Aleksey <kozyatinskiy@chromium.org>
2018-07-28 10:28:15 -0700

    fix(backend): ndd agent cleanup (#74)
    - node does not get 100% CPU anymore when waiting for a connection,
    - 'Stay attached' checkbox works for profiling,
    - simplified ndd agent.
    
    Fixes #73

--- node_debug_demon/client.js
+++ node_debug_demon/client.js
@@ -4,6 +4,7 @@
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
  */
 
+const chokidar = require('chokidar');
 const EventEmitter = require('events');
 const fs = require('fs');
 const path = require('path');
@@ -17,115 +18,62 @@ class Client extends EventEmitter {
     super();
     this._nddStore = nddStore;
     this._nddStoreWatcher = null;
-    this._watchEventListener = this._onWatchEvent.bind(this);
-
     this._instances = new Map();
-    this._updateScheduled = new Set();
   }
 
   async start() {
-    this._nddStoreWatcher = fs.watch(this._nddStore);
-    this._nddStoreWatcher.on('change', this._watchEventListener);
-    await this.cleanup();
-    await this._fetchExisting();
+    this._nddStoreWatcher = chokidar.watch([this._nddStore], {
+      awaitWriteFinish: {
+        stabilityThreshold: 500,
+        pollInterval: 100
+      },
+      cwd: this._nddStore,
+      depth: 0,
+      ignorePermissionErrors: true
+    });
+    this._nddStoreWatcher.on('add', path => {
+      const match = path.match(/([0-9]+)-ready/);
+      if (!match)
+        return;
+      const id = match[1];
+      let instance = this._instances.get(id);
+      if (instance)
+        return;
+      instance = new NodeProcess(id, this._nddStore);
+      this._instances.set(id, instance);
+      this.emit('added', instance);
+    });
+    this._nddStoreWatcher.on('unlink', path => {
+      const match = path.match(/([0-9]+)-ready/);
+      if (!match)
+        return;
+      const id = match[1];
+      const instance = this._instances.get(id);
+      if (!instance)
+        return;
+      this._instances.delete(id);
+      this.emit('finished', instance);
+    });
   }
 
   async stop() {
     const fileNames = await util.promisify(fs.readdir)(this._nddStore);
     await Promise.all(fileNames.map(async fileName => {
       const match = fileName.match(/([0-9]+)-ready/);
-      if (match) {
-        const [_, id] = match;
-        process.kill(id, 'SIGKILL');
-      }
+      if (match)
+        process.kill(match[1], 'SIGKILL');
     }));
-    await this.cleanup();
-    await this.dispose();
-  }
-
-  async cleanup() {
-    const fileNames = await util.promisify(fs.readdir)(this._nddStore);
-    await Promise.all(fileNames.map(async fileName => {
-      const match = fileName.match(/([0-9]+)-ready/);
-      if (match) {
-        const [_, id] = match;
-        let exists = true;
-        try {
-          process.kill(id, 0);
-        } catch (e) {
-          exists = false;
-        }
-        if (!exists)
-          await util.promisify(fs.unlink)(path.join(this._nddStore, fileName)).catch(() => 0);
-      }
-    }));
-  }
-
-  dispose() {
-    this._updateScheduled.clear();
     this._instances.clear();
     if (this._nddStoreWatcher) {
-      this._nddStoreWatcher.removeListener('change', this._watchEventListener);
       this._nddStoreWatcher.close();
+      this._nddStoreWatcher = null;
     }
   }
 
-  async _fetchExisting() {
-    const fileNames = await util.promisify(fs.readdir)(this._nddStore);
-    for (const fileName of fileNames)
-      this._onWatchEvent('rename', fileName);
-  }
-
-  /**
-   * @param {string} eventType
-   * @param {string} fileName
-   */
-  _onWatchEvent(eventType, fileName) {
-    if (eventType === 'rename') {
-      const match = fileName.match(/([0-9]+)-ready/);
-      if (match) {
-        const [_, id] = match;
-        this._scheduleUpdate(id);
-      }
-    }
-  }
-
-  /**
-   * @param {string} id
-   */
-  _scheduleUpdate(id) {
-    if (!this._updateScheduled.has(id)) {
-      setImmediate(() => this._update(id));
-      this._updateScheduled.add(id);
-    }
-  }
-
-  /**
-   * @param {string} id
-   */
-  async _update(id) {
-    this._updateScheduled.delete(id);
-
-    const files = await util.promisify(fs.readdir)(this._nddStore);
-    const re = new RegExp(`${id}-ready`);
-    let isFinished = true;
-    for (const file of files) {
-      const match = file.match(re);
-      if (match) {
-        isFinished = false;
-        break;
-      }
-    }
-    if (this._instances.has(id) && isFinished) {
-      const instance = this._instances.get(id);
-      instance._finished();
-      this._instances.delete(id);
-      this.emit('finished', instance);
-    } else if (!isFinished) {
-      const instance = new NodeProcess(id, this._nddStore);
-      this._instances.set(id, instance);
-      this.emit('added', instance);
-    }
+  detach(id) {
+    const name = path.join(this._nddStore, `${id}-ready`);
+    if (fs.existsSync(name))
+      util.promisify(fs.unlink)(name);
   }
 }
 
@@ -139,16 +87,6 @@ class NodeProcess {
 
     this._id = id;
     this._info = null;
-    this._isFinished = false;
-
-    this._finishedCallback = null;
-  }
-
-  /**
-   * @return {boolean}
-   */
-  isFinished() {
-    return this._isFinished;
   }
 
   /**
@@ -156,17 +94,6 @@ class NodeProcess {
    */
   id() {
     return this._id;
-  }
-
-  async kill() {
-    if (!this._isFinished) {
-      const done = new Promise(resolve => this._finishedCallback = resolve);
-      try {
-        process.kill(this._id, 'SIGKILL');
-      } catch (e) {
-      }
-      await done;
-    }
   }
 
   /**
@@ -185,10 +112,11 @@ class NodeProcess {
     return this._info;
   }
 
-  _finished() {
-    this._isFinished = true;
-    if (this._finishedCallback)
-      this._finishedCallback();
+  kill() {
+    try {
+      process.kill(Number(this._id), 'SIGKILL');
+    } catch (e) {
+    }
   }
 }
 
