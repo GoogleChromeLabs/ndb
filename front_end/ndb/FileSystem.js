4895867274bf846049a23bf56cc42ef4c13b738f
Aleksei Koziatinskii <ak239spb@gmail.com>
2018-11-12 14:20:19 -0800

    feat(carlo,fs): migrate fs to separate service based on carlo rpc

---
+++ front_end/ndb/FileSystem.js
@@ -0,0 +1,257 @@
+/**
+ * @license Copyright 2018 Google Inc. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+ */
+
+Ndb.FileSystem = class extends Persistence.PlatformFileSystem {
+  constructor(manager, rootPath, rootURL) {
+    super(rootURL, '');
+    this._rootURL = rootURL;
+    this._embedderPath = rootPath;
+    this._manager = manager;
+
+    /** @type {!Array<string>} */
+    this._initialFilePaths = [];
+    /** @type {!Array<string>} */
+    this._initialGitFolders = [];
+    /** @type {!Array<string>} */
+    this._excludedFolders = [];
+
+    this._servicePromise = null;
+  }
+
+  static async create(manager, rootPath, rootURL) {
+    const fs = new Ndb.FileSystem(manager, rootPath, rootURL);
+    await fs._initFilePaths();
+    return fs;
+  }
+
+  /**
+   * @override
+   * @return {string}
+   */
+  embedderPath() {
+    return this._embedderPath;
+  }
+
+  /**
+   * @override
+   * @return {!Promise}
+   */
+  async _initFilePaths() {
+    const excludePattern = this._manager.workspaceFolderExcludePatternSetting().get();
+    const service = await this._service();
+    await service.startWatcher(this._rootURL, rpc.handle(this));
+    const result = await service.filePaths(this._rootURL, excludePattern);
+    this._initialFilePaths = result.filePaths;
+    this._initialGitFolders = result.gitFolders;
+    this._excludedFolders = result.excludedFolders;
+  }
+
+  /**
+   * @return {!Promise}
+   */
+  async _service() {
+    if (!this._servicePromise) {
+      const [info, backend] = await Promise.all([getProcessInfo(), Runtime.backendPromise]);
+      this._servicePromise = backend.createService(info.serviceDir + '/file_system.js');
+    }
+    return this._servicePromise;
+  }
+
+  /**
+   * @override
+   * @return {!Array<string>}
+   */
+  initialFilePaths() {
+    return this._initialFilePaths;
+  }
+
+  /**
+   * @override
+   * @return {!Array<string>}
+   */
+  initialGitFolders() {
+    return this._initialGitFolders;
+  }
+
+  /**
+   * @override
+   * @param {string} path
+   * @return {!Promise<?{modificationTime: !Date, size: number}>}
+   */
+  getMetadata(path) {
+    // This method should never be called as long as we are matching using file urls.
+    throw new Error('not implemented');
+  }
+
+  /**
+   * @override
+   * @param {string} path
+   * @return {!Promise<?Blob>}
+   */
+  requestFileBlob(path) {
+    throw new Error('not implemented');
+  }
+
+  /**
+   * @override
+   * @param {string} path
+   * @param {function(?string,boolean)} callback
+   */
+  async requestFileContent(path, callback) {
+    const result = await (await this._service()).readFile(this._rootURL + path, 'utf8');
+    callback(result, false);
+  }
+
+  /**
+   * @override
+   * @param {string} path
+   * @param {string} content
+   * @param {boolean} isBase64
+   */
+  async setFileContent(path, content, isBase64) {
+    console.log(this._rootURL + path);
+    await (await this._service()).writeFile(this._rootURL + path, content, isBase64 ? 'base64' : 'utf8');
+  }
+
+  /**
+   * @override
+   * @param {string} path
+   * @param {?string} name
+   * @return {!Promise<?string>}
+   */
+  async createFile(path, name) {
+    // TODO(ak239): we should decide where to do substr here or on backend side.
+    const result = await (await this._service()).createFile(this._rootURL + (path.length === 0 || path.startsWith('/') ? '' : '/') + path);
+    return result.substr(this._rootURL.length + 1);
+  }
+
+  /**
+   * @override
+   * @param {string} path
+   * @return {!Promise<boolean>}
+   */
+  async deleteFile(path) {
+    return await (await this._service()).deleteFile(this._rootURL + path);
+  }
+
+  /**
+   * @override
+   * @param {string} path
+   * @param {string} newName
+   * @param {function(boolean, string=)} callback
+   */
+  async renameFile(path, newName, callback) {
+    const result = await (await this._service()).renameFile(this._rootURL + path, newName);
+    callback(result, result ? newName : null);
+  }
+
+  /**
+   * @override
+   * @param {string} path
+   * @return {!Common.ResourceType}
+   */
+  contentType(path) {
+    const extension = Common.ParsedURL.extractExtension(path);
+    if (Persistence.IsolatedFileSystem._styleSheetExtensions.has(extension))
+      return Common.resourceTypes.Stylesheet;
+    if (Persistence.IsolatedFileSystem._documentExtensions.has(extension))
+      return Common.resourceTypes.Document;
+    if (Persistence.IsolatedFileSystem.ImageExtensions.has(extension))
+      return Common.resourceTypes.Image;
+    if (Persistence.IsolatedFileSystem._scriptExtensions.has(extension))
+      return Common.resourceTypes.Script;
+    return Persistence.IsolatedFileSystem.BinaryExtensions.has(extension) ? Common.resourceTypes.Other :
+                                                                            Common.resourceTypes.Document;
+  }
+
+  /**
+   * @override
+   * @param {string} path
+   * @return {string}
+   */
+  mimeFromPath(path) {
+    return Common.ResourceType.mimeFromURL(path) || 'text/plain';
+  }
+
+  /**
+   * @override
+   * @param {string} path
+   * @return {boolean}
+   */
+  canExcludeFolder(path) {
+    return !!path ;
+  }
+
+  /**
+   * @override
+   * @param {string} url
+   * @return {string}
+   */
+  tooltipForURL(url) {
+    const path = Common.ParsedURL.urlToPlatformPath(url, Host.isWin()).trimMiddle(150);
+    return ls`Linked to ${path}`;
+  }
+
+  /**
+   * @override
+   * @param {string} query
+   * @param {!Common.Progress} progress
+   * @return {!Promise<!Array<string>>}
+   */
+  searchInPath(query, progress) {
+    return new Promise(resolve => {
+      const requestId = this._manager.registerCallback(innerCallback);
+      InspectorFrontendHost.searchInPath(requestId, this._embedderPath, query);
+
+      /**
+       * @param {!Array<string>} files
+       */
+      function innerCallback(files) {
+        resolve(files.map(path => Common.ParsedURL.platformPathToURL(path)));
+        progress.worked(1);
+      }
+    });
+  }
+
+  /**
+   * @param {string} name
+   */
+  added(name) {
+    InspectorFrontendAPI.fileSystemFilesChangedAddedRemoved([], [name], []);
+  }
+
+  /**
+   * @param {string} name
+   */
+  changed(name) {
+    InspectorFrontendAPI.fileSystemFilesChangedAddedRemoved([name], [], []);
+  }
+
+  /**
+   * @param {string} name
+   */
+  removed(name) {
+    InspectorFrontendAPI.fileSystemFilesChangedAddedRemoved([], [], [name]);
+  }
+
+  /**
+   * @override
+   * @param {!Common.Progress} progress
+   */
+  indexContent(progress) {
+    progress.setTotalWork(1);
+    const requestId = this._manager.registerProgress(progress);
+    InspectorFrontendHost.indexPath(requestId, this._embedderPath, JSON.stringify(this._excludedFolders));
+  }
+
+  /**
+   * @override
+   * @return {boolean}
+   */
+  supportsAutomapping() {
+    return true;
+  }
+}
---
+++ services/file_system.js
@@ -0,0 +1,153 @@
+/**
+ * @license Copyright 2018 Google Inc. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+ */
+
+const { rpc, rpc_process } = require('carlo/rpc');
+const chokidar = require('chokidar');
+const fs = require('fs');
+const path = require('path');
+const { URL, pathToFileURL, fileURLToPath } = require('url');
+
+class FileSystemHandler {
+  constructor() {
+    this._watcher = null;
+  }
+  /**
+   * @param {string} fileURL
+   * @param {string} excludePattern
+   */
+  filePaths(fileURL, excludePattern) {
+    let excludeRegex = null;
+    try {
+      excludeRegex = new RegExp(excludePattern); 
+    } catch (e) {
+    }
+    const queue = [new URL(fileURL)];
+    const visited = new Set();
+
+    const filePaths = new Set();
+    const gitFolders = new Set();
+    const excludedFolders = new Set();
+
+    while (queue.length) {
+      const url = queue.shift();
+      if (visited.has(url)) continue;
+      visited.add(url);
+
+      try {
+        const names = fs.readdirSync(url);
+        const urlString = url.toString();
+        if (this._watcher) this._watcher.add(urlString);
+        names.forEach(name => {
+          if (name === '.git') gitFolders.add(urlString.substr(fileURL.length + 1));
+          const fileName = urlString + '/' + name;
+          if (excludeRegex && excludeRegex.test(fileName.substr(fileURL.length))) {
+            excludedFolders.add(fileName.substr(fileURL.length + 1));
+          } else {
+            queue.push(new URL(fileName));
+          }
+        });
+      } catch (e) {
+        if (e.code !== 'ENOTDIR')
+          throw e;
+        const urlString = url.toString();
+        if (excludeRegex && excludeRegex.test(urlString)) continue;
+        filePaths.add(urlString.substr(fileURL.length + 1));
+      }
+    }
+    return {
+      excludedFolder: Array.from(excludedFolders),
+      gitFolders: Array.from(gitFolders),
+      filePaths: Array.from(filePaths)
+    };
+  }
+
+  /**
+   * @param {string} fileURL
+   * @param {string} encoding
+   */
+  readFile(fileURL, encoding) {
+    return fs.readFileSync(new URL(fileURL), encoding);
+  }
+
+  /**
+   * @param {string} fileURL
+   * @param {string} content
+   * @param {string} encoding
+   */
+  writeFile(fileURL, content, encoding) {
+    if (encoding === 'base64') {
+      content = Buffer.from(content, 'base64');
+    }
+    fs.writeFileSync(new URL(fileURL), content, {encoding: encoding});
+  }
+
+  /**
+   * @param {string} folderURL
+   */
+  createFile(folderURL) {
+    let name = 'NewFile';
+    let counter = 1;
+    while (fs.existsSync(new URL(folderURL + '/' + name))) {
+      name = 'NewFile' + counter;
+      ++counter;
+    }
+    fs.writeFileSync(new URL(folderURL + '/' + name), '');
+    return folderURL + '/' + name;
+  }
+
+  /**
+   * @param {string} fileURL
+   */
+  deleteFile(fileURL) {
+    try {
+      fs.unlinkSync(new URL(fileURL));
+      return true;
+    } catch (e) {
+      return false;
+    }
+  }
+
+  /**
+   * @param {string} fileURL
+   * @param {string} newName
+   */
+  renameFile(fileURL, newName) {
+    const newURL = new URL(fileURL.substr(0, fileURL.lastIndexOf('/') + 1) + newName);
+    try {
+      if (fs.existsSync(newURL)) return false;
+      fs.renameSync(new URL(fileURL), newURL);
+      return true;
+    } catch (e) {
+      return false;
+    }
+  }
+
+  startWatcher(rootURL, client) {
+    const watcher = chokidar.watch([fileURLToPath(rootURL)], {
+      ignoreInitial: true,
+      awaitWriteFinish: {
+        stabilityThreshold: 500,
+        pollInterval: 100
+      },
+      depth: 0,
+      ignorePermissionErrors: true
+    });
+    watcher.on('error', console.error);
+    watcher.on('all', (event, name) => {
+      if (event === 'add' || event === 'addDir')
+        client.added(name);
+      else if (event === 'change')
+        client.changed(name);
+      else if (event === 'unlink' || event === 'unlinkDir')
+        client.removed(name);
+    });
+    this._watcher = watcher;
+  }
+}
+
+rpc_process.init(args => {
+  return rpc.handle(new FileSystemHandler());
+});
